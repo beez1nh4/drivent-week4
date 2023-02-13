f66555a58c127114decd0109a9c6ac6311d24d39
Melissa <melmihomel@gmail.com>
2023-02-13 11:32:27 -0300

    feat: add booking router

--- src/routers/bookings-router.ts
+++ src/routers/bookings-router.ts
@@ -1,1 +1,26 @@
 import { Router } from "express";
+import { authenticateToken } from "@/middlewares";
+import { getBooking, postBooking, updateBooking } from "@/controllers";
+
+const bookingsRouter = Router();
+
+bookingsRouter
+  .all("/*", authenticateToken)
+  .get("/", getBooking)
+  .post("/", postBooking)
+  .put("/:bookingId", updateBooking);
+
+export { bookingsRouter };
+
+
+
+
+
+
+
+
+
+
+
+
+
